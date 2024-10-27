f0033ea3d3a103547c4fe4559102ffed5b634896
Eric Hoover <ehoover3@yahoo.com>
2024-10-27 07:57:43 -0400

    update tab title

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Eric Hoover - GenAI",
+  title: "Generative AI",
   description: "Generative AI application",
 };
 
