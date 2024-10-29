f4efcd43b78c2c94f3908a85c9e8bbcbb063dbcc
Eric Hoover <ehoover3@yahoo.com>
2024-10-28 22:12:12 -0400

    center auth login horizontally

--- app/(auth)/(routes)/login/[[...login]]/page.tsx
+++ app/(auth)/(routes)/login/[[...login]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className='flex justify-center p-5'>
+    <div>
       <SignIn />
     </div>
   );
--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className='flex justify-center p-5'>
+    <div>
       <SignUp />
     </div>
   );
